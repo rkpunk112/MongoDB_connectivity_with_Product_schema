773b3e22027ed01a0793640588f55ad3a1dd6958
RoHiT <126324979+rkpunk112@users.noreply.github.com>
2024-07-05 21:56:00 +0530

    index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const route=require('./route/people')
 
 mongoose
   .connect(
-    "mongodb+srv://rk9190743:vmv04p6rr313xhKp@backenddb.ru9xtiw.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+  "mongodb+srv://rk9190743:<password>@backenddb.ru9xtiw.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
   )
   .then(() => {
     console.log("Connected To database");
@@ -16,7 +16,6 @@ mongoose
     console.log("Didn't able to Connect");
   });
 
-  // mongodb+srv://rk9190743:<password>@backenddb.ru9xtiw.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
